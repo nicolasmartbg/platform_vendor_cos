0a5a319e37f11c3d5f471717344059aa97d61db1
Nicola Nicolov <nicola-smart@hotmail.com>
2018-07-22 19:59:41 +0300

    Simplix Orange 1.7.3  Your branch is up to date with 'origin/oreo'.  Changes to be committed: 	modified:   common.mk 	modified:   prebuilt/common/app/SimplixCenter.apk 	modified:   vendorsetup.sh  Untracked files: 	themes/Fonts/assets/fonts/Nioki.zip

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/Cosmic-OS/platform_vendor_cos/pulsar-release/cos.devices  | sed -e 's/#.*$//' | awk '{printf "simplix_%s-userdebug\n", $1}')
+for combo in $(curl -s https://raw.githubusercontent.com/SimplixDot/platform_vendor_cos/oreo/simplix.devices  | sed -e 's/#.*$//' | awk '{printf "simplix_%s-userdebug\n", $1}')
 do
     add_lunch_combo $combo
 done
